fac7d5876f142b999e128ac0ef7c14f5cc874b61
Vayun Shyam <52934413+vash483@users.noreply.github.com>
2021-05-14 09:30:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 pygame.font.init()
 
 WIDTH, HEIGHT = 800, 800
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Fruit Dash")
 
 # Load images
@@ -62,8 +62,8 @@ class Player(Monkey):
 class Enemy(Monkey):
 
 	COLOR_MAP = {
-				"red" : (RED_SPACE_SHIP, RED_LASER)
-				"green" : (GREEN_SPACE_SHIP, GREEN_LASER)
+				"red" : (RED_SPACE_SHIP, RED_LASER),
+				"green" : (GREEN_SPACE_SHIP, GREEN_LASER),
 				"blue" : (BLUE_SPACE_SHIP, BLUE_LASER)
 	}
 	def __init__(self, x, y, color, health=100):
@@ -71,13 +71,13 @@ class Enemy(Monkey):
 		self.monkey_img, self.laser_img = self.COLOR_MAP[color]
 		self.mask = pygame.mask.from_surface(self.monkey_img)
 
-	def move(self, vel)
+	def move(self, vel):
 		self.y += vel
 
 
 
 def main():
-    runGame = True
+    run = True
     FPS = 60
     level = 1
     lives = 5
@@ -85,47 +85,68 @@ def main():
 
     enemies = []
     wave_length = 5
+    enemy_vel = 1
     
     player_vel = 5
 
     player = Player(300, 650)
 
-    gameClock = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
-    def win_redraw():
-    	WINDOW.blit(BACKGROUND,(0,0))
+    def redraw_window():
+    	WIN.blit(BACKGROUND,(0,0))
     	lives_label = game_font.render(f"Lives: {lives}", 1, (255,255,255))
     	level_label = game_font.render(f"Level: {level}", 1, (255,255,255))
 
-    	WINDOW.blit(level_label,(10,10))
-    	WINDOW.blit(lives_label,(WIDTH - level_label.get_width() - 10,10))
+    	WIN.blit(level_label,(10,10))
+    	WIN.blit(lives_label,(WIDTH - level_label.get_width() - 10,10))
+'''
 
-    	player.draw(WINDOW)
+        for enemy in enemies:
+            enemy.draw(WIN)
 
+    	player.draw(WIN)
+
+
+        for
     	pygame.display.update()
+'''
 
-    while runGame:
+while run:
         #ensure game constantly runs
-        gameClock.tick(FPS)
-        win_redraw()
+    clock.tick(FPS)
+
+    if len(enemies) == 0:
+        level += 1
+        wave_length += 5
+        for i in range(wave_length):
+            enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+            enemies.append(enemy)
+
+        
 
         #run Game
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                runGame = False
-                pygame.quit()
-                sys.exit()
-                exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+            sys.exit()
+            exit()
 
-        key_controls = pygame.key.get_pressed()
-        if key_controls[pygame.K_a]and player.x - player_vel > 0: #moving left
-        	player.x -= player_vel
-        if key_controls[pygame.K_d]and player.x + player_vel + player.get_width() < WIDTH: #moving right
-        	player.x += player_vel
-        if key_controls[pygame.K_w]and player.y - player_vel > 0: #moving up
-        	player.y -= player_vel
-        if key_controls[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: #moving down
-        	player.y += player_vel
+    key_controls = pygame.key.get_pressed()
+    if key_controls[pygame.K_a]and player.x - player_vel > 0: #moving left
+        player.x -= player_vel
+    if key_controls[pygame.K_d]and player.x + player_vel + player.get_width() < WIDTH: #moving right
+        player.x += player_vel
+    if key_controls[pygame.K_w]and player.y - player_vel > 0: #moving up
+        player.y -= player_vel
+    if key_controls[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: #moving down
+        player.y += player_vel
+
+    for enemy in enemies:
+        enemy.move(enemy_vel)
+
+    redraw_window()
 
                
 main()
